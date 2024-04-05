b2b6749fcd1b84fce6d76239a639c2ee37938005
Jozef Reisinger <jozef.reisinger@gmail.com>
2024-04-05 08:34:23 +0200

    stats working

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io"
 	"io/fs"
@@ -14,13 +15,32 @@ import (
 	"strings"
 	"sync"
 	"text/tabwriter"
+	"time"
+)
+
+const (
+	katasFile = "gokatas.json"
+	reposURL  = "https://api.github.com/orgs/gokatas/repos"
 )
 
 func main() {
 	log.SetPrefix(os.Args[0] + ": ")
 	log.SetFlags(0)
 
-	katas, err := get()
+	stats := flag.Bool("stats", false, "show your statistics")
+	done := flag.String("done", "", "you've just done `kata`")
+	flag.Parse()
+
+	if *stats {
+		ss, err := getStats(katasFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		printStats(ss)
+		os.Exit(0)
+	}
+
+	katas, err := getKatas(reposURL)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -40,8 +60,78 @@ func main() {
 	}
 	wg.Wait()
 
+	if *done != "" {
+		var found bool
+		for _, k := range katas {
+			if k.Name == *done {
+				found = true
+				break
+			}
+		}
+		if !found {
+			log.Fatalf("no such kata: %s", *done)
+		}
+
+		if err := storeStats(*done, katasFile); err != nil {
+			log.Fatal(err)
+		}
+
+		os.Exit(0)
+	}
+
 	sort.Sort(byGoLines(katas))
 	print(katas)
+}
+
+type Stats map[string][]time.Time
+
+func printStats(ss Stats) {
+	const format = "%v\t%v\t%v\n"
+	tw := new(tabwriter.Writer).Init(os.Stdout, 0, 8, 2, ' ', 0)
+	fmt.Fprintf(tw, format, "Kata", "Done", "Last")
+	fmt.Fprintf(tw, format, "----", "----", "----")
+	for kata, dones := range ss {
+		fmt.Fprintf(tw, format, kata, fmt.Sprintf("%dx", len(dones)), dones[len(dones)-1].Format("2006-01-02 15:04:05"))
+	}
+	tw.Flush()
+}
+
+func storeStats(kata, file string) error {
+	fi, err := os.Stat(file)
+	if err != nil {
+		return err
+	}
+	var stats Stats
+	var in []byte
+	if fi.Size() > 0 {
+		in, err = os.ReadFile(file)
+		if err != nil {
+			return err
+		}
+		if err := json.Unmarshal(in, &stats); err != nil {
+			return err
+		}
+	} else {
+		stats = make(map[string][]time.Time)
+	}
+	stats[kata] = append(stats[kata], time.Now())
+	data, err := json.Marshal(stats)
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(file, data, 0644)
+}
+
+func getStats(file string) (Stats, error) {
+	data, err := os.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+	var ss Stats
+	if err := json.Unmarshal(data, &ss); err != nil {
+		return nil, err
+	}
+	return ss, nil
 }
 
 func cloneAndCount(k kata) (lines int, err error) {
@@ -136,8 +226,7 @@ func print(katas []kata) {
 	tw.Flush()
 }
 
-func get() ([]kata, error) {
-	url := "https://api.github.com/orgs/gokatas/repos"
+func getKatas(url string) ([]kata, error) {
 	resp, err := http.Get(url)
 	if err != nil {
 		return nil, err
