c9a6a08feb786cd688b47c17ea84046476917710
Jozef Reisinger <jozef.reisinger@gmail.com>
2024-09-09 08:28:39 +0200

    make variable local

--- report.go
+++ report.go
@@ -12,8 +12,6 @@ type Boundary struct {
 	Until time.Time
 }
 
-var sixEmptySpaces = strings.Repeat(" ", 6)
-
 func buildHeader(start, end time.Time) string {
 	s := strings.Builder{}
 	for current := start; current.Before(end) || current.Equal(end); current = current.AddDate(0, 1, 0) {
@@ -59,6 +57,7 @@ func printReport(katas Katas, b Boundary) {
 		b.Until = b.Until.AddDate(0, 0, 1)
 	}
 
+	sixEmptySpaces := strings.Repeat(" ", 6)
 	fmt.Printf("%s     %s\n", sixEmptySpaces, buildHeader(b.Since, b.Until))
 
 	s := strings.Builder{}
